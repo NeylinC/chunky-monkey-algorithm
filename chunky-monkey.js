20fc1322fabb8e1b7a82bbb863b7936265c17e8f
Neylin Castillo <neylincastillo@gmail.com>
2025-06-03 13:59:16 -0400

    Update chunky-monkey.js
    Fixed format

--- chunky-monkey.js
+++ chunky-monkey.js
@@ -1,17 +1,16 @@
 let array = [];
 let remainingInArray = [];
 
-function chunkArrayInGroups (array, num) {
+function chunkArrayInGroups(array, num) {
 
-let timesToRun = Math.ceil(array.length/num)
+    let timesToRun = Math.ceil(array.length / num)
 
-for(let i = 0; i < timesToRun; i++)
-{
-remainingInArray.push(array.splice(0,num))
-}
-console.log(remainingInArray)
+    for (let i = 0; i < timesToRun; i++) {
+        remainingInArray.push(array.splice(0, num))
+    }
+    console.log(remainingInArray)
 
-return remainingInArray
+    return remainingInArray
 
 }
 
