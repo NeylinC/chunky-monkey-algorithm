2cdd922d4c7405a8de2c25d40c735c0b6b443cd0
Neylin Castillo <neylincastillo@gmail.com>
2025-07-02 10:39:45 -0400

    Update chunky-monkey.js
    removed console log

--- chunky-monkey.js
+++ chunky-monkey.js
@@ -2,15 +2,10 @@ let array = [];
 let remainingInArray = [];
 
 function chunkArrayInGroups(array, num) {
-
     let timesToRun = Math.ceil(array.length / num)
-
     for (let i = 0; i < timesToRun; i++) {
         remainingInArray.push(array.splice(0, num))
     }
-    console.log(remainingInArray)
-
     return remainingInArray
-
 }
 
